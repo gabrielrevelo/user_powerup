d455d6836c373456d9d6b712c49e23d48c83f985
GaRev-Dev <revelo.gabriel.38@gmail.com>
2023-05-04 11:18:01 -0500

    Validated required fields and format on DTO for account creation

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestController.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/controller/UserRestController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +32,7 @@ public class UserRestController {
                 @ApiResponse(responseCode = "409", description = "Person already exists",
                         content = @Content(mediaType = "application/json", schema = @Schema(ref = "#/components/schemas/Error")))})
     @PostMapping
-    public ResponseEntity<Map<String, String>> savePerson(@RequestBody UserRequestDto userRequestDto) {
+    public ResponseEntity<Map<String, String>> savePerson(@Valid @RequestBody UserRequestDto userRequestDto) {
         personHandler.saveUser(userRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(Collections.singletonMap(Constants.RESPONSE_MESSAGE_KEY, Constants.PERSON_CREATED_MESSAGE));
--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
@@ -1,16 +1,31 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request;
 
+import com.pragma.powerup.usermicroservice.configuration.Constants;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UserRequestDto {
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
     private String name;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
     private String surname;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Email(message = Constants.INVALID_FORMAT_MESSAGE)
     private String mail;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Pattern(regexp = "^\\+?[0-9]{1,12}$", message = Constants.INVALID_FORMAT_MESSAGE)
     private String phone;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Pattern(regexp = "\\d*", message = Constants.INVALID_FORMAT_MESSAGE)
     private String dniNumber;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
     private String password;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = Constants.INVALID_FORMAT_MESSAGE)
     private String dateOfBirth;
 }
