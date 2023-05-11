7fc5724afa0b8c593349376cc409c3cd3f43c4f1
GaRev-Dev <revelo.gabriel.38@gmail.com>
2023-05-11 13:41:06 -0500

    feat: Update UserRequestDto with Swagger documentation, document validation and email validation
    - Updated UserRequestDto with Swagger documentation for the fields.
    - Modified email validation to ensure a valid email format.
    - Changed the document identification validation to allow 6 to 10 numeric digits.

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/UserRequestDto.java
@@ -1,8 +1,7 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request;
 
-import com.pragma.powerup.usermicroservice.adapters.driving.http.exceptions.UserUnderAgeException;
 import com.pragma.powerup.usermicroservice.configuration.Constants;
-import jakarta.validation.constraints.Email;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
@@ -12,28 +11,35 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
-import java.time.Period;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "User DTO")
 public class UserRequestDto {
     @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Schema(description = "Name", example = "Juan")
     private String name;
     @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Schema(description = "Surname", example = "Gomez")
     private String surname;
     @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
-    @Email(message = Constants.INVALID_FORMAT_MESSAGE)
+    @Pattern(regexp = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = Constants.INVALID_FORMAT_MESSAGE)
+    @Schema(description = "Email", example = "juangomez@email.com")
     private String mail;
     @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
-    @Pattern(regexp = "^\\+?[0-9]{10,12}$", message = Constants.INVALID_FORMAT_MESSAGE)
+    @Pattern(regexp = "^\\+?[0-9]{12}$", message = Constants.INVALID_FORMAT_MESSAGE)
+    @Schema(description = "Cellphone Number", example = "+573001234567")
     private String phone;
     @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
-    @Pattern(regexp = "\\d*", message = Constants.INVALID_FORMAT_MESSAGE)
+    @Pattern(regexp = "^\\d{6,10}$", message = Constants.INVALID_FORMAT_MESSAGE)
+    @Schema(description = "Identification Document Number", example = "1234567890")
     private String dniNumber;
     @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Schema(description = "Password", example = "Password123!")
     private String password;
     @Past(message = Constants.DATE_CANNOT_BE_PAST_MESSAGE)
+    @Schema(description = "Date of Birth", example = "2000-02-20")
     private LocalDate dateOfBirth;
 }
